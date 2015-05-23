bcff5dd86a97326bb67242822b54dc0a6a923080
Atlanssia <hi@atlanssia.com>
2015-05-23 09:05:27 +0800

    update

--- aralog.go
+++ aralog.go
@@ -3,16 +3,29 @@ package aralog
 import (
 	"runtime"
 	"fmt"
+	"io"
+	"sync"
 )
+
+type Logger struct {
+	mutex sync.Mutex // ensures atomic writes; protects the following fields
+	prefix string     // prefix to write at beginning of each line
+	flag int        // properties
+	out io.Writer  // destination for output
+	buf []byte     // for accumulating text to write
+}
 
 func init() {
 }
 
-func Log(calldepth int, s string) {
+func Log(callDepth int, s string) {
 
-	counter, file, line, ok := runtime.Caller(calldepth)
+	_, file, line, ok := runtime.Caller(callDepth)
+	if !ok {
+		file = "???"
+		line = 0
+	}
 
-	fmt.Println("counter:", counter)
 	fmt.Println("file:", file)
 	fmt.Println("line:", line)
 	fmt.Println("ok:", ok)
