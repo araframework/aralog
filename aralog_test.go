12a34a94e50dc7886d17a808a210071aed5f88e0
Manfred Wong <hi@atlanssia.com>
2015-10-25 01:54:23 +0800

    Update aralog_test.go

--- aralog_test.go
+++ aralog_test.go
@@ -2,11 +2,10 @@ package aralog
 
 import (
 	"testing"
-	"github.com/araframework/aralog"
 )
 
 func TestAraLog(t *testing.T) {
-	logger, err := aralog.NewFileLogger("ara.log", aralog.Llongfile | aralog.Ltime)
+	logger, err := NewFileLogger("ara.log", Llongfile | Ltime)
 	if err != nil {
 		t.Error("new logger error: ", err)
 	}
